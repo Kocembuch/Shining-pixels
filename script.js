2a5432343aa40e0d07b6d169785cfad4e3a37809
Joanna Romek <narwa1@interia.pl>
2020-05-24 20:18:10 +0200

    complete project

---
+++ script.js
@@ -0,0 +1,22 @@
+const pixels = [...document.querySelectorAll("div")];
+const pixelsOdd = [...document.querySelectorAll("div:nth-child(odd)")];
+
+
+
+function changePixel() {
+    const pixel = Math.floor(Math.random() * pixels.length);
+    const pixelOdd = Math.floor(Math.random() * pixelsOdd.length)
+
+    console.log(pixel);
+    pixels[pixel].classList.toggle("pixelAnimated");
+    pixelsOdd[pixelOdd].classList.toggle("pixelAnimated2");
+
+    setTimeout(() => {
+        console.log('działą');
+        pixels[pixel].classList.toggle("pixelAnimated");
+        pixelsOdd[pixelOdd].classList.toggle("pixelAnimated2");
+    }, 1500)
+
+}
+
+setInterval(changePixel, 1000);
